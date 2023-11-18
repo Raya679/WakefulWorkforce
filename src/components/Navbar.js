dfb22c60acd1659c1a4220357aace3de7ef76543
richa-sawant <rssawant_b22@ce.vjti.ac.in>
2023-11-19 01:47:09 +0530

    Homepage code added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav className='sticky top-0'>
-      <div className='flex items-center px-2 py-3 font-bold bg-slate-900 text-white'>
+      <div className='flex items-center px-2 py-2 font-bold bg-slate-900 text-white'>
         <button className='name mx-5  text-4xl '>WAKEFUL WORKFORCE</button>
         <button className='flex-grow'></button>
         <button type="button" className='mx-4 text-1xl hover:bg-indigo-900 rounded-md px-4 py-2 underline underline-offset-8 decoration-cyan-400'>HOME</button>
         <button type="button" className='mx-4 text-1xl hover:bg-indigo-900 rounded-md px-4 py-2'>ABOUT US</button>
         <button type="button" className='mx-4 text-1xl hover:bg-indigo-900 rounded-md px-4 py-2'>CONTACT US</button>
-        <button type="button" className='mx-4 text-1xl hover:bg-indigo-900 p-1 font-extrabold border-4 border-cyan-400 rounded-[10px] px-4 py-2 '>LOGIN</button>
+        <button type="button" className='mx-4 text-1xl hover:bg-indigo-900 p-1 font-extrabold border-4 border-cyan-400 rounded-[10px] px-4 py-2 text-2xl '>LOGIN</button>
       </div>
       {/* Additional content here */}
     </nav>

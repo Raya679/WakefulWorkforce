89906b828468f435d1b4c9f2e7fd3465c9a4a463
Advait Yadav <eulerandfriends@gmail.com>
2024-01-10 10:45:40 +0530

    minor 0

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -29,38 +29,38 @@ export default function Simple() {
         }
     }
 
-    if (isAuth == "true") {
+    if (isAuth === "true") {
         return (<AboutUs />)
     }
 
     return (
         <div className=" flex items-center justify-center text-white h-screen">
-            <div className="flex items-center justify-center p-10 rounded-xl gap-3 bg-[#282c34] bg-opacity-[70%] flex-col ">
-                <div className="  font-fedroka text-xl text-left font-medium ">
+            <div className="flex py-3 px-5 rounded-xl gap-3 bg-[#282c34] bg-opacity-[70%] flex-col">
+                <div className=" text-3xl font-medium my-3 ">
                     SIGN UP
                 </div>
-                <button className=" cursor-pointer flex font-nunito border-3 border-cyan-400 rounded-3xl py-1 px-3">Continue with Google</button>
-                <div className=" font-nunito text-gray-400 ">----or sign in with email----</div>
-                <div>
-                    <form onSubmit={formSubmit} className=" flex gap-3 flex-col">
-                        <div className="flex flex-col gap-2">
+                <button className=" cursor-pointer font-nunito border-3 border-cyan-400 rounded-3xl py-1 px-3 ">Continue with Google</button>
+                <div className=" font-nunito text-gray-400 text-center">----or sign in with email----</div>
+                <div className=" self-start">
+                    <form onSubmit={formSubmit} className=" flex gap-3 flex-col py-2 items-start">
+                        <div className="flex flex-col gap-2 w-full">
                             <label htmlFor="email" className=" font-nunito ">Email : </label>
                             <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} className=" form-input text-black rounded-md" required />
                         </div>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-2 w-full">
                             <label htmlFor="pass" className=" font-nunito ">Password : </label>
                             <input type="text" value={password} onChange={(e) => { setPass(e.target.value) }} className=" form-input text-black rounded-md" required />
                         </div>
 
-                        <div className="flex content-between gap-3">
+                        <div className="flex content-between gap-5 py-2">
                             <div className="flex">
-                                <input type="checkbox" className=" form-checkbox rounded bg-cyan-400 text-white cursor-pointer " />
+                                <input type="checkbox" className=" form-checkbox rounded bg-cyan-400 text-white cursor-pointer mx-1" />
                                 <p className=" font-nunito text-xs">Remember Me?</p>
                             </div>
                             <Link to="/" className=" font-nunito text-cyan-400 text-xs cursor-pointer">Forgot password?</Link>
                         </div>
 
-                        <button className=" cursor-pointer bg-cyan-400 text-[#282c34] font-fedroka font-semibold rounded-2xl py-1" type="submit">Submit</button>
+                        <button className=" cursor-pointer self-center bg-cyan-400 text-[#282c34] font-fedroka font-semibold rounded-2xl px-4 py-1" type="submit">Submit</button>
                     </form>
                 </div>
 

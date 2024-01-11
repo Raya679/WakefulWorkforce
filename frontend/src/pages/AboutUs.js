b7aab0a1d4a7dc06f19c6bfaea3d7388c0729277
Anushka <anushkasy2003@gmail.com>
2024-01-11 23:20:09 +0530

    changes

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -31,28 +31,28 @@ const AboutUs = () => {
             <div className="w-full h-2/5 bg-slate-900 flex-col">
             <div className="flex items-center justify-center text-6xl font-bold w-full mb-12">Meet Our Team</div>
             <div className="flex items-center justify-center w-full">
-            <a href="#" className="w-60 h-auto rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900 ">
+            <a href="https://www.linkedin.com/in/advait-yadav-231154271/" className="w-60 h-1/2 rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900 ">
                     <div className="flex items-center justify-center my-5">
                     <img className="" src="./Mask group (2).png" alt="" />
                     </div>
                     <div className="text-center my-3 font-bold text-2xl">Advait Yadav</div>
                     <div className="p-3 text-1xl text-center"><p>Cursus sit amet dictum sit amet justo. Tortor condimentum lacinia quis vel eros donec ac odio tempor. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum.</p></div>
                 </a>
-                <a href="#" className="w-60 h-auto rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900">
+                <a href="https://www.linkedin.com/in/raya-chakravarty/" className="w-60 h-1/2 rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900">
                     <div className="flex items-center justify-center my-5">
                     <img className="" src="./Mask group.png" alt="" />
                     </div>
                     <div className="text-center my-3 font-bold text-2xl">Raya Chakravarty</div>
                     <div className="p-3 text-1xl text-center"><p>Cursus sit amet dictum sit amet justo. Tortor condimentum lacinia quis vel eros donec ac odio tempor. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum.</p></div>
                 </a>
-                <a href="#" className="w-60 h-auto rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900">
+                <a href="https://www.linkedin.com/in/richasawant20/" className="w-60 h-1/2 rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900">
                     <div className="flex items-center justify-center my-5">
                     <img className="" src="./Mask group3.png" alt="" />
                     </div>
                     <div className="text-center my-3 font-bold text-2xl">Richa Sawant</div>
                     <div className="p-3 text-1xl text-center"><p>Cursus sit amet dictum sit amet justo. Tortor condimentum lacinia quis vel eros donec ac odio tempor. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum.</p></div>
                 </a>
-                <a href="#" className="w-60 h-auto rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900">
+                <a href="https://www.linkedin.com/in/anushka-yadav-356b20276/" className="w-60 h-1/2 rounded-lg border-1 border-white mx-20 my-24 hover:bg-cyan-400 hover:text-slate-900">
                     <div className="flex items-center justify-center my-5">
                     <img className="" src="./Mask group (1).png" alt="" />
                     </div>

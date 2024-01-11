c14e3a35cd85135929c7365b46f4067081b1fa4a
Anushka <anushkasy2003@gmail.com>
2024-01-11 17:12:06 +0530

    changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,13 +7,13 @@ export default function Navbar() {
       <div className='flex items-center px-2 py-2 font-bold bg-slate-900 text-white'>
         <button className='name mx-5  text-4xl '>WAKEFUL WORKFORCE</button>
         <button className='flex-grow'></button>
-        <Link to="/" className='mx-4 text-1xl hover:bg-indigo-900 rounded-md px-4 py-2 underline underline-offset-8 decoration-cyan-400'>HOME</Link>
-        <Link to="/aboutus" className='mx-4 text-1xl hover:bg-indigo-900 rounded-md px-4 py-2'>ABOUT US</Link>
-        <button type="button" className='mx-4 text-1xl hover:bg-indigo-900 rounded-md px-4 py-2'>CONTACT US</button>
+        <Link to="/" className='mx-8 text-2xl hover:text-cyan-400 hover:text-3xl'>HOME</Link>
+        <Link to="/aboutus" className='mx-8 text-2xl hover:text-cyan-400 hover:text-3xl '>ABOUT US</Link>
+        <button type="button" className='mx-8 text-2xl hover:text-cyan-400 hover:text-3xl'>CONTACT US</button>
         {localStorage.getItem("jwt") ?
           <Link to="/form" className='mx-4 text-1xl hover:bg-indigo-900 p-1 font-extrabold border-4 border-cyan-400 rounded-[10px] px-4 py-2 text-2xl '>LOGOUT</Link>
           :
-          <Link to="/form" className='mx-4 text-1xl hover:bg-indigo-900 p-1 font-extrabold border-4 border-cyan-400 rounded-[10px] px-4 py-2 text-2xl '>LOGIN</Link>
+          <Link to="/form" className='mx-4 text-1xl hover:bg-cyan-400 hover:text-slate-900 p-1 font-extrabold border-2 border-cyan-400 rounded-[10px] px-3 py-2 text-2xl '>LOGIN</Link>
         }
       </div>
     </nav>

3c82f2dfcda95c7e58f85fc3f8e44d7e262f90c1
Anushka <anushkasy2003@gmail.com>
2024-01-16 18:02:33 +0530

    changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,13 +23,13 @@ export default function Navbar() {
       <div className='flex items-center px-2 py-2 font-bold bg-slate-900 text-white'>
         <button className='name mx-5  text-4xl '>WAKEFUL WORKFORCE</button>
         <button className='flex-grow'></button>
-        <Link to="/" className='mx-10 text-1xl font-light hover:font-bold '>HOME</Link>
-        <Link to="/aboutus" className='mx-10 text-1xl font-light hover:font-bold  '>ABOUT US</Link>
-        <Link to="/contactus" className='mx-10 text-1xl font-light hover:font-bold '>CONTACT US</Link>
+        <Link to="/" className='mx-10 text-1xl  font-bold  hover:text-cyan-400 transition ease-out duration-500  '>HOME</Link>
+        <Link to="/aboutus" className='mx-10 text-1xl  font-bold hover:text-cyan-400 transition ease-out duration-500 '>ABOUT US</Link>
+        <Link to="/contactus" className='mx-10 text-1xl font-bold hover:text-cyan-400 transition ease-out duration-500'>CONTACT US</Link>
         {isAuthenticated ?
           <Link to="/login" className='mx-4 text-1xl hover:bg-indigo-900 p-1 font-bold border-4 border-cyan-400 rounded-[10px] px-4 py-2 text-2xl ' onClick={async () => { await handleLogout() }}>LOGOUT</Link>
           :
-          <Link to="/login" className='mx-4 text-1xl hover:bg-cyan-400 hover:text-slate-900 p-1 font-bold border-4 border-cyan-400 rounded-[26px] px-4 py-2 text-1xl '>LOGIN</Link>
+          <Link to="/login" className='mx-4 text-1xl hover:bg-cyan-400 hover:text-slate-900 p-1 font-bold border-4 border-cyan-400 rounded-[26px] px-4 py-2 text-1xl transition ease-out duration-300'>LOGIN</Link>
         }
       </div>
     </nav>

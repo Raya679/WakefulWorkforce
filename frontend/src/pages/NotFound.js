7f97caedabd4147dcc1d79516d46974987cfce8d
richa-sawant <rssawant_b22@ce.vjti.ac.in>
2024-02-08 01:12:50 +0530

    fixes and profile page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -3,10 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <>
-            <div className='text-white text-center text-5xl mt-16 font-bold'>
-                Page Not Found
-            </div>
-            <div className=' w-1/2 mx-auto'>
+            <div className=' w-1/4 mx-auto mt-40'>
             <img src="/404.png" alt="my" />
             </div>
         </>

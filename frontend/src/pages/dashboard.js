c27ef132d24d934cf8baafd439ae2d8124ad9bfc
richa-sawant <rssawant_b22@ce.vjti.ac.in>
2024-02-08 00:21:52 +0530

    fixes and profile page

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -10,14 +10,14 @@ const Dashboard = () => {
         return <Navigate to='/login' />;
     }
 
-    const today = new Date().getDate(); // Get today's date
+    const today = new Date().getDate(); 
 
     return (
         <div className="bg-slate-900 min-h-screen p-6 ">
             <div className="grid grid-cols-2 gap-6 mt-3">
                 <div className="my-14 ml-32 col-span-2 flex flex-col border-cyan-400 border-2 p-6 gap-6 group bg-black rounded-3xl w-5/6 shadow-2xl shadow-cyan-600">
                     <div className="flex items-center ">
-                        <p className='text-cyan-400 mt-16 px-6 font-nunito font-bold basis-3/4 ml-12 text-9xl'>{today}</p> {/* Display today's date */}
+                        <p className='text-cyan-400 mt-16 px-6 font-nunito font-bold basis-3/4 ml-12 text-9xl'>{today}</p> 
                         <div className='flex flex-col my-3'>
                             <p className='font-oswald font-bold text-3xl text-end my-3 mr-7 text-white'>TASK FOR TODAY</p>
                             <p className="text-gray-400 text-lg font-nunito font-medium text-end mr-7">
@@ -25,7 +25,7 @@ const Dashboard = () => {
                             </p>
                         </div>
                     </div>
-                    <button className="bg-sky-950 text-2xl font-bold hover:bg-cyan-500 py-2 px-4 rounded-[30px] border-4 border-cyan-400 ml-auto mb-4 -mt-3 mr-7 transition ease-out duration-300 ">Start session</button>
+                    <Link to="http://127.0.0.1:5000/" className=" bg-cyan-400 text-slate-900 font-extrabold font-['Fredoka One'] text-xl py-2 px-4 rounded-full self-end  hover:bg-slate-900 hover:border-1 hover:text-cyan-400 hover:border-cyan-400 transition ">START SESSION </Link>
                 </div>
                 <Link to="/todo">
                     <div className="flex items-center justify-center my-8 h-40 bg-black border-cyan-400 border-2 p-6 gap-3 hover:cursor-pointer rounded-3xl w-3/4 ml-44">
